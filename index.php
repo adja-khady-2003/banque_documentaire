b6c2e17e823af44f652c59b4dbefa7f4917834b2
adja-khady-2003 <adjakhadyn@gmail.com>
2023-09-01 22:22:44 +0000

    liste doc

--- index.php
+++ index.php
@@ -90,9 +90,12 @@ $query_part = mysqli_query($conn, $sql_part) or die(mysqli_error($conn));
     <div class="w3-container w3-content w3-center w3-padding-64" style="max-width:800px" id="band">
       <h2 class="w3-wide">BANQUE DOCUMENTAIRE</h2>
       <p class="w3-opacity"><i>à savoir</i></p>
-      <p class="w3-justify">We have created a fictional band website. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-        ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum consectetur
-        adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+      <p class="w3-justify"> Cette base de documents recueille des descriptions de livres de tout genre.
+      Une banque documentaire comporte une base de données et un logiciel, assurant la création de nouvelles données,
+       la mise à jour des informations déjà existantes et la possibilité d'interrogation.
+       Ainsi,la base de données est concrètement le programme informatique qui est donc "le contenant". 
+       La banque de données est le "contenu" de la base et dans une base de données il peut y avoir plusieurs banques.
+      </p>
     </div>
 
     <!-- The Tour Section -->
